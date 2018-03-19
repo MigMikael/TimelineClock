5d433ec8bf6251f100b1cb809b953cdce4d222d4
Chanachai Puttaruksa <Chanachai_mig@hotmail.com>
2018-03-19 09:51:47 +0700

    change status text

--- app/Notifications/TweetTime.php
+++ app/Notifications/TweetTime.php
@@ -3,17 +3,12 @@
 namespace App\Notifications;
 
 use Carbon\Carbon;
-use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use NotificationChannels\Twitter\TwitterChannel;
 use NotificationChannels\Twitter\TwitterStatusUpdate;
 
 class TweetTime extends Notification
 {
-    use Queueable;
-
     /**
      * Create a new notification instance.
      *
@@ -37,33 +32,6 @@ class TweetTime extends Notification
 
     public function toTwitter($notifiable)
     {
-        return new TwitterStatusUpdate(Carbon::now());
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
+        return new TwitterStatusUpdate("some text here");
     }
 }
