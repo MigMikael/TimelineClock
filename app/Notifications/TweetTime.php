25c4a7343eef1b32be1faaf8a76274fc2493fbc5
Chanachai Puttaruksa <Chanachai_mig@hotmail.com>
2018-03-20 20:59:21 +0700

    tweet big text

--- app/Notifications/TweetTime.php
+++ app/Notifications/TweetTime.php
@@ -4,6 +4,7 @@ namespace App\Notifications;
 
 use Carbon\Carbon;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\Twitter\Exceptions\CouldNotSendNotification;
 use NotificationChannels\Twitter\TwitterChannel;
 use NotificationChannels\Twitter\TwitterStatusUpdate;
 
@@ -32,6 +33,163 @@ class TweetTime extends Notification
 
     public function toTwitter($notifiable)
     {
-        return new TwitterStatusUpdate(Carbon::now()->format('H:i'));
+        $timeStr = Carbon::now()->format('H:i');
+        $split_time = explode(':', $timeStr);
+        $hour = $split_time[0];
+        $bigText = $this->genBigText($hour);
+
+        try {
+            return new TwitterStatusUpdate($bigText);
+        } catch (CouldNotSendNotification $e) {
+
+        }
+    }
+
+    public function genBigText($str)
+    {
+        if ($str == '01'){
+            return '▄▀ ▀▄ ▄█─ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+█─ ─█ ─█─ 　 ─ 　 █─ ─█ █─ ─█ 
+▀▄ ▄▀ ▄█▄ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '02'){
+            return '▄▀ ▀▄ █▀█ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+█─ ─█ ─▄▀ 　 ─ 　 █─ ─█ █─ ─█ 
+▀▄ ▄▀ █▄▄ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '03'){
+            return '▄▀ ▀▄ █▀▀█ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+█─ ─█ ──▀▄ 　 ─ 　 █─ ─█ █─ ─█ 
+▀▄ ▄▀ █▄▄█ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '04'){
+            return '▄▀ ▀▄ ─█▀█─ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+█─ ─█ █▄▄█▄ 　 ─ 　 █─ ─█ █─ ─█ 
+▀▄ ▄▀ ───█─ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '05'){
+            return '▄▀ ▀▄ █▀▀ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+█─ ─█ ▀▀▄ 　 ─ 　 █─ ─█ █─ ─█ 
+▀▄ ▄▀ ▄▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '06'){
+            return '▄▀ ▀▄ ▄▀▀▄ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+█─ ─█ █▄▄─ 　 ─ 　 █─ ─█ █─ ─█ 
+▀▄ ▄▀ ▀▄▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '07'){
+            return '▄▀ ▀▄ ▀▀▀█ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+█─ ─█ ──█─ 　 ─ 　 █─ ─█ █─ ─█ 
+▀▄ ▄▀ ─▐▌─ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '08'){
+            return '▄▀ ▀▄ ▄▀▀▄ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+█─ ─█ ▄▀▀▄ 　 ─ 　 █─ ─█ █─ ─█ 
+▀▄ ▄▀ ▀▄▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '09'){
+            return '▄▀ ▀▄ ▄▀▀▄ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+█─ ─█ ▀▄▄█ 　 ─ 　 █─ ─█ █─ ─█ 
+▀▄ ▄▀ ─▄▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '10'){
+            return '▄█─ ▄▀ ▀▄ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─█─ █─ ─█ 　 ─ 　 █─ ─█ █─ ─█ 
+▄█▄ ▀▄ ▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '11'){
+            return '▄█─ ▄█─ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─█─ ─█─ 　 ─ 　 █─ ─█ █─ ─█ 
+▄█▄ ▄█▄ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '12'){
+            return '▄█─ █▀█ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─█─ ─▄▀ 　 ─ 　 █─ ─█ █─ ─█ 
+▄█▄ █▄▄ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '13'){
+            return '▄█─ █▀▀█ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─█─ ──▀▄ 　 ─ 　 █─ ─█ █─ ─█ 
+▄█▄ █▄▄█ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '14'){
+            return '▄█─ ─█▀█─ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─█─ █▄▄█▄ 　 ─ 　 █─ ─█ █─ ─█ 
+▄█▄ ───█─ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '15'){
+            return '▄█─ █▀▀ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─█─ ▀▀▄ 　 ─ 　 █─ ─█ █─ ─█ 
+▄█▄ ▄▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '16'){
+            return '▄█─ ▄▀▀▄ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─█─ █▄▄─ 　 ─ 　 █─ ─█ █─ ─█ 
+▄█▄ ▀▄▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '17'){
+            return '▄█─ ▀▀▀█ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─█─ ──█─ 　 ─ 　 █─ ─█ █─ ─█ 
+▄█▄ ─▐▌─ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '18'){
+            return '▄█─ ▄▀▀▄ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─█─ ▄▀▀▄ 　 ─ 　 █─ ─█ █─ ─█ 
+▄█▄ ▀▄▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '19'){
+            return '▄█─ ▄▀▀▄ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─█─ ▀▄▄█ 　 ─ 　 █─ ─█ █─ ─█ 
+▄█▄ ─▄▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '20'){
+            return '█▀█ ▄▀ ▀▄ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─▄▀ █─ ─█ 　 ─ 　 █─ ─█ █─ ─█ 
+█▄▄ ▀▄ ▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '21'){
+            return '█▀█ ▄█─ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─▄▀ ─█─ 　 ─ 　 █─ ─█ █─ ─█ 
+█▄▄ ▄█▄ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '22'){
+            return '█▀█ █▀█ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─▄▀ ─▄▀ 　 ─ 　 █─ ─█ █─ ─█ 
+█▄▄ █▄▄ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        elseif ($str == '23'){
+            return '█▀█ █▀▀█ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+─▄▀ ──▀▄ 　 ─ 　 █─ ─█ █─ ─█ 
+█▄▄ █▄▄█ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
+        else{
+            return '▄▀ ▀▄ ▄▀ ▀▄ 　 ▄ 　 ▄▀ ▀▄ ▄▀ ▀▄ 
+█─ ─█ █─ ─█ 　 ─ 　 █─ ─█ █─ ─█ 
+▀▄ ▄▀ ▀▄ ▄▀ 　 ▀ 　 ▀▄ ▄▀ ▀▄ ▄▀ 
+';
+        }
     }
 }
